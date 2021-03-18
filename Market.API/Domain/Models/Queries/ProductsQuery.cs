4e823201d1756f7ec22128a8845cf03ca0234e3c
stescobedo92 <stescobedo.31@gmail.com>
2021-03-18 09:37:19 -0400

    ProductsQuery for operations in Products

---
+++ Market.API/Domain/Models/Queries/ProductsQuery.cs
@@ -0,0 +1,12 @@
+namespace Market.API.Domain.Models.Queries
+{
+    public class ProductsQuery : Query
+    {
+        public int? CategoryId { get; set; }
+
+        public ProductsQuery(int? categoryId, int page, int itemsPerPage) : base(page, itemsPerPage)
+        {
+            CategoryId = categoryId;
+        }
+    }
+}
