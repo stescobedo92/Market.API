5a5d449af42601b7b4aaa7a22b344e2fda5f4840
stescobedo92 <stescobedo.31@gmail.com>
2021-03-18 09:19:55 -0400

    Added the Category Model

---
+++ Market.API/Domain/Models/Category.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Market.API.Domain.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Product> Products { get; set; } = new List<Product>();
+    }
+}
